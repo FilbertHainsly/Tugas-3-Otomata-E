f0c495bdc6419545e414e24823cc3b5c2ad65eee
FilbertHM <74392047+FilbertHainsly@users.noreply.github.com>
2025-03-23 12:34:19 +0700

    memberi comment pada setiap line

--- DFA_simulator.py
+++ DFA_simulator.py
@@ -1,18 +1,23 @@
-import json
+import json  # Mengimpor library JSON untuk membaca file JSON
 
+# Membuka dan membaca file JSON yang berisi definisi DFA
 with open('input.json', 'r') as f:
-	system = json.load(f)
-	
-	all_states = system['states']
-	curr_state = system['start_state']
-	accept_states = system['accept_states']
-	transitions = system['transitions']
-	test = system['test_string']
-	
+	system = json.load(f)  # Mengubah isi file JSON menjadi dictionary (kamus) di Python
+
+	# Mengambil informasi DFA dari file JSON
+	all_states = system['states'] # Daftar semua state
+	curr_state = system['start_state'] # State awal
+	accept_states = system['accept_states'] # Daftar state yang menerima (accepting states)
+	transitions = system['transitions'] # Transisi antar state berdasarkan input
+	test = system['test_string'] # String yang akan diuji
+
+	# Mencetak state awal dalam jalur DFA
 	print(f'Path: {curr_state}', end='')
 
+	# Melakukan iterasi setiap karakter dalam test_string
 	for c in test:
-		curr_state = transitions[curr_state][c]
-		print(f' -> {curr_state}', end='')
+		curr_state = transitions[curr_state][c] # Pindah ke state berikutnya berdasarkan transisi DFA
+		print(f' -> {curr_state}', end='') Mencetak jalur state yang dilewati
 	
-	print(f"\nStatus: {'ACCEPTED' if curr_state in accept_states else 'REJECTED'}")
+	print(f"\nStatus: {'ACCEPTED' if curr_state in accept_states else 'REJECTED'}") Jika state akhir ada di accept_states akan diterima, jika tidak maka akan ditolak
+
